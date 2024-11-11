88dd6987bb6e2e1deb8af4a50996e3c951b2502a
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-11-11 08:17:49 -0500

    Add project files.

---
+++ F24Week10DisconnectedModel/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace F24Week10DisconnectedModel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
